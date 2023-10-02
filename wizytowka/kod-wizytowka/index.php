f4b621704933bf1434bac57f0ed4e7df982b2f0a
Czarek <102460281+CzarekZadrozny@users.noreply.github.com>
2023-10-02 08:36:59 +0200

    2.10.23(dodanie zmnian)

---
+++ wizytowka/kod-wizytowka/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wizytowka</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    <header>
+        <div class="baner">
+            <h1>Wizytowki pracownikow</h1>
+
+            <form method="post">
+                <input type="number" name="idPracownika" id="idPracownika" value="1" min="1" max="9">
+                <button type="submit">WYŚWIETL</button>
+            </form>
+        </div>
+    </header>
+
+    <main>
+        <div class="wizytowka">
+            <?php 
+            $con = new mysqli('localhost','root','','')
+            
+            
+            
+            
+            ?>
+        </div>
+    </main>
+    
+</body>
+</html>

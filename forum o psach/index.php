8c9c19626af0b9be3abd79a22284397876871cf5
Czarek <102460281+CzarekZadrozny@users.noreply.github.com>
2023-10-08 21:37:00 +0200

    dodanie pracy 8.10.23

---
+++ forum o psach/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl-Pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+<div class="baner">
+    <h1>Forum miłosnikow psów</h1>
+</div>
+    </header>
+
+    <main>
+
+<div class="lewy">
+
+    <img src="Avatar.jpg" alt="Użytkownik forum">
+
+    <?php
+     
+    $con = new mysqli('localhost','root','','forumpsy');
+    $zap = 
+
+    ?>
+</div>
+<div class="prawy"></div>
+
+    </main>
+
+    <footer>
+<div class="stopka"></div>
+    </footer>
+    
+</body>
+</html>

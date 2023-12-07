fb1d5ff2e5dd8958c28b7df242fba820559ad047
Czarek <102460281+CzarekZadrozny@users.noreply.github.com>
2023-12-07 10:26:58 +0100

    dodanie

---
+++ vet/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weterynarz 24/7</title>
+</head >
+<body style="background-color: ; color: white;">
+    <header>
+        <div class="baner1" style="background-color: rgb(202,202,202);"    >
+            <img src="logo2.PNG" alt="logo">
+        </div>
+    </header>
+    
+</body>
+</html>

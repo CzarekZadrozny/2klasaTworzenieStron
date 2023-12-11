4c80b8b81e2f743e285a3a249af3e7bed60ef6c9
Czarek <102460281+CzarekZadrozny@users.noreply.github.com>
2023-12-11 08:50:05 +0100

    s

--- vet/index.php
+++ vet/index.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weterynarz 24/7</title>
-</head >
-<body style="background-color: ; color: white;">
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
     <header>
-        <div class="baner1" style="background-color: rgb(202,202,202);"    >
-            <img src="logo2.PNG" alt="logo">
+        <div class="baner1"> 
+           
+            <h1>Gabinet weterynaryjny FAUNA</h1> 
+           
+        </div>
+        <div class="baner2">
+            <label for="zap">
+            <input type="text" name="zap" id="zap" placeholder="Szukaj..."><img src="search.png" class="search"></label>
         </div>
     </header>
     

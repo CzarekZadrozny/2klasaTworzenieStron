fbfa83aaff62046421308fcb1f2e4e76025c7890
Czarek <Cezaryzadrozny11@gmail.com>
2024-05-06 13:12:45 +0200

    +

--- Firma wykonujaca sieci komputerowe/sigma.js
+++ Firma wykonujaca sieci komputerowe/sigma.js
@@ -1,8 +1,5 @@
-function Aktualizuj(id) {
-    var x = parseInt(prompt("Podaj ilosc"));
-    document.getElementById(id).innerHTML = x;
-    sprawdz();
-}
+var liczbaZamowien = 1
+var NoweId = 5
 
 function sprawdz() {
     const collection = document.getElementsByClassName("wartosc");
@@ -16,3 +13,30 @@ function sprawdz() {
         }
     }
 }
+
+function Aktualizuj(id) {
+    var x = parseInt(prompt("Podaj ilosc"));
+    document.getElementById(id).innerHTML = x;
+    sprawdz();
+}
+
+function Zamow(id){
+    alert("Zamowienie nr: "+ liczbaZamowien)
+    liczbaZamowien ++
+    Ilosc = document.getElementById(id).innerHTML
+    nowaIlosc = Ilosc - 1;
+    document.getElementById(id).innerHTML = nowaIlosc
+    sprawdz();
+}
+
+function Nowa(){
+    var nazwa = prompt("Podaj nazwe produku")
+    var typ = prompt("Podaj jednostke")
+    var wiersz = document.createElement("tr");
+    wiersz.innerHTML ='<tr class="tabelka"> <td class="tabelka">'+ nazwa +'</td> <td class="tabelka">'+ typ +'</td> <td class="wartosc" id="'+NoweId+'">0</td> <td class="tabelka"><button onclick="Aktualizuj('+NoweId+')">Aktualizuj</button></td> <td class="tabelka"><button onclick="Zamow('+NoweId+')">Zamów</button></td> </tr>'
+    var dodanie = document.getElementById('table');
+    dodanie.appendChild(wiersz);
+    NoweId ++;
+    sprawdz()
+
+}         

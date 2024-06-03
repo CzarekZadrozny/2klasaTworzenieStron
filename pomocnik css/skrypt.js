3f1fc9b711ac1d45ad8c317dc8f608676a0075ea
Czarek <Cezaryzadrozny11@gmail.com>
2024-06-03 11:44:34 +0200

    !

--- pomocnik css/skrypt.js
+++
@@ -1,9 +0,0 @@
-function generatePalette() {
-    var hueValue = document.getElementById('hue-input').value;
-    var saturationValues = [100, 80, 60, 40, 20];
-    var colorCells = document.getElementsByClassName('color-cell');
-    for (var i = 0; i < colorCells.length; i++) {
-      colorCells[i].style.backgroundColor = 'hsl(' + hueValue + ', ' + saturationValues[i] + '%, 50%)';
-    }
-  }
-  
